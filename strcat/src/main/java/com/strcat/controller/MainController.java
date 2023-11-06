c52999a3c44c829fcd07eda0519cb81c53910335
Sumin Lee <suminn0707@gmail.com>
2023-11-06 14:55:18 +0900

    test: 문자열 수정 테스트

--- strcat/src/main/java/com/strcat/controller/MainController.java
+++ strcat/src/main/java/com/strcat/controller/MainController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String helloSpring() {
-        return "hello spring!!";
+        return "Hello spring!!!";
     }
 }
