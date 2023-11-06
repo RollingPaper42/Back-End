9f7df59e0add37e3a4da13269007f74efdda693a
seongjunkim <tjdwns5063@naver.com>
2023-11-06 15:33:55 +0900

    fix: 디렉토리 구조 변경

---
+++ src/main/java/com/strcat/StrcatApplication.java
@@ -0,0 +1,13 @@
+package com.strcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrcatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrcatApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/strcat/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.strcat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String helloSpring() {
+        return "Hello jayoon!!!";
+    }
+}
