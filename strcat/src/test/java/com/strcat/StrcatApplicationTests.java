1a2789a3d8089a41449586cfce8dec31856082cd
Sumin Lee <suminn0707@gmail.com>
2023-11-06 14:26:12 +0900

    init: create spring project

---
+++ strcat/src/test/java/com/strcat/StrcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrcatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
