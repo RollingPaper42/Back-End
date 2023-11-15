ea9763d935e4cb3fb543fc37ff501c877f2506bd
brixxt27 <brixxt27@gmail.com>
2023-11-15 20:33:48 +0900

    feat: User entity 관계 설정 및 변경
    - 날짜 자동 생성 추가
    - wrapper class 로 변경

--- src/main/java/com/strcat/domain/User.java
+++ src/main/java/com/strcat/domain/User.java
@@ -1,25 +1,34 @@
 package com.strcat.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.List;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Data
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long id;
-    @Column(name = "created_at")
+    Long id;
+
+    @CreatedDate
+    @Column(name ="created_at", nullable = false)
     LocalDateTime createdAt;
 
-    public User(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Board> boards;
 }
