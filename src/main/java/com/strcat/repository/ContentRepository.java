0e330a4cae8ea66148925b5eec40bc883c4a905a
brixxt27 <brixxt27@gmail.com>
2023-11-15 21:49:35 +0900

    feat: 레포지토리 구현 및 수정
    - Board, Content 구현
    - User, OAuthUser override 메소드 삭제

---
+++ src/main/java/com/strcat/repository/ContentRepository.java
@@ -0,0 +1,8 @@
+package com.strcat.repository;
+
+import com.strcat.domain.Content;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContentRepository extends JpaRepository<Content, Long> {}
