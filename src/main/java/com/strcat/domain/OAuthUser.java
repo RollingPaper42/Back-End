b12815cd0f24a7310b696e912dc3b98f69241846
brixxt27 <brixxt27@gmail.com>
2023-11-15 20:35:02 +0900

    feat: OAuthUser entity 관계 설정 및 변경
    - 날짜 자동 생성 추가
    - wrapper class 로 변경

--- src/main/java/com/strcat/domain/OAuthUser.java
+++ src/main/java/com/strcat/domain/OAuthUser.java
@@ -1,5 +1,6 @@
 package com.strcat.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,12 +16,17 @@ import lombok.NoArgsConstructor;
 public class OAuthUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long id;
+    Long id;
+
+    @Column(nullable = false)
+    Integer provider;
+
+    @Column(nullable = false)
+    String oauthId;
+
     @OneToOne
     @JoinColumn(name = "user_id")
     User user;
-    int provider;
-    String oauthId;
 
     public OAuthUser(User user, int provider, String oauthId) {
         this.user = user;
