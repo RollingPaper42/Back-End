4d8f4c47e3e490cf281203d2b6bab2260b1b7e78
brixxt27 <brixxt27@gmail.com>
2023-11-16 13:26:11 +0900

    fix: OAuthUserRepository 복원

--- src/main/java/com/strcat/repository/OAuthUserRepository.java
+++ src/main/java/com/strcat/repository/OAuthUserRepository.java
@@ -4,4 +4,6 @@ import com.strcat.domain.OAuthUser;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OAuthUserRepository extends JpaRepository<OAuthUser, Long> {}
+public interface OAuthUserRepository extends JpaRepository<OAuthUser, Long> {
+    Optional<OAuthUser> findByOauthIdAndProvider(String oauthId, int provider);
+}
