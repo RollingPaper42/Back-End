b31b590599e8d566e76f80d99c4761aa0fa59d4b
seongjunkim <tjdwns5063@naver.com>
2023-11-17 22:10:18 +0900

    feat: JwtAuthenticationEntryPoint 구현
    Spring Security에서 인증이 실패한 경우, 에러가 처리되는 JwtAuthenticationEntryPoint 구현

--- src/main/java/com/strcat/exception/ResponseException.java
+++
@@ -1,9 +0,0 @@
-package com.strcat.exception;
-
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-public class ResponseException extends Exception {
-    public final Integer statusCode;
-    public final String message;
-}
--- src/main/java/com/strcat/exception/UnauthorizedException.java
+++
@@ -1,6 +0,0 @@
-package com.strcat.exception;
-
-import jakarta.servlet.ServletException;
-
-public class UnauthorizedException extends ServletException {
-}
