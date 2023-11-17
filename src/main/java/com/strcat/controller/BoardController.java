c3fc8a276b2a780c1c3eb0445e088d75038d4e0b
brixxt27 <brixxt27@gmail.com>
2023-11-17 13:01:04 +0900

    feat: content 생성 컨트롤러 핸들러 생성
    - /board 라는 공통 리소스 내에서 content를 생성하기 때문에 board 와 content 생성을 컨트롤러 단위에서 나눌지 합칠지 고민했다. 유지 보수를 위해 나누는 게 좋아 보이는데 처음에는 전부 모아 구현한 뒤, 이후에 나누기로 했다.

---
+++ src/main/java/com/strcat/controller/BoardController.java
@@ -0,0 +1,21 @@
+package com.strcat.controller;
+
+import com.strcat.dto.CreateContentReqDto;
+import com.strcat.service.ContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller("/board")
+@RequiredArgsConstructor
+public class BoardController {
+    private final ContentService contentService;
+
+    @PostMapping("/{boardId}/content")
+    public void createContent(@PathVariable(name = "boardId") String encryptedBoardId,
+                              @RequestBody CreateContentReqDto dto) {
+//        contentService.create(dto, boardId);
+    }
+}
