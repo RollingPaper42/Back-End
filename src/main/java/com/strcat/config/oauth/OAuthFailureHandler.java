64d85cfbbb3247f33f9abb88d4cc176a0867c93d
brixxt27 <brixxt27@gmail.com>
2023-11-17 15:45:32 +0900

    feat: SecurityConfig OAuthFailureHandler 추가

---
+++ src/main/java/com/strcat/config/oauth/OAuthFailureHandler.java
@@ -0,0 +1,22 @@
+package com.strcat.config.oauth;
+
+import com.strcat.exception.UnauthorizedException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class OAuthFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+        log.info("login failure");
+        throw new UnauthorizedException();
+    }
+}
