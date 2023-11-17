b05cf1cdd19ed64c46bd877eb4e8426b472b518e
Sumin Lee <suminn0707@gmail.com>
2023-11-17 22:18:02 +0900

    feat: 암호화 로직 수정
    - 기존의 방식은 iv(initialization vector)가 지정되지 않아 secretkey가 바뀌는 방식이라 decrypt 하지 못함
    - 따라서 iv를 지정해두는 방식으로 변경함
    - 서버 재실행 후에도 키가 바뀌지 않는 것을 확인함

--- src/main/java/com/strcat/util/AesSecretUtils.java
+++ src/main/java/com/strcat/util/AesSecretUtils.java
@@ -1,33 +1,44 @@
 package com.strcat.util;
 
-import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import javax.crypto.Cipher;
-import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 
 @Component
 public class AesSecretUtils {
-    private final SecretKey secretKey;
-    private final String ALGORITHM = "AES";
+
+    private final String key;
+    private final String iv;
+    private final String ALGORITHM = "AES/CBC/PKCS5Padding";
 
     public AesSecretUtils(@Value("${jwt.secret}") String secretKey) {
-        byte[] encoded = Base64.getEncoder().encode(secretKey.getBytes());
-        this.secretKey = Keys.hmacShaKeyFor(encoded);
+        this.key = secretKey;
+        this.iv = secretKey.substring(0, 16);
     }
 
     public String encrypt(Long data) throws Exception {
         Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        return new String(cipher.doFinal(data.toString().getBytes(StandardCharsets.UTF_8)));
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes());
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParameterSpec);
+
+        byte[] encodedBytes = cipher.doFinal(data.toString().getBytes());
+        byte[] encrypted = Base64.getEncoder().encode(encodedBytes);
+        return new String(encrypted).trim();
     }
 
     public Long decrypt(String encryptedData) throws Exception {
         Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
-        byte[] decryptedBytes = cipher.doFinal(encryptedData.getBytes());
-        return Long.parseLong(new String(decryptedBytes, StandardCharsets.UTF_8));
+        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes());
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParameterSpec);
+
+        byte[] decodedBytes = Base64.getDecoder().decode(encryptedData.getBytes());
+        byte[] decrypted = cipher.doFinal(decodedBytes);
+        return Long.parseLong(new String(decrypted, StandardCharsets.UTF_8));
     }
 }
