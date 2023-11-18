8e36609576199fb2cdc64ba3ed0415524aa36f26
Sumin Lee <suminn0707@gmail.com>
2023-11-18 15:53:39 +0900

    refactor: 메서드를 추가해 중복 코드 삭제

--- src/main/java/com/strcat/util/AesSecretUtils.java
+++ src/main/java/com/strcat/util/AesSecretUtils.java
@@ -20,23 +20,23 @@ public class AesSecretUtils {
         this.iv = secretKey.substring(0, 16);
     }
 
-    public String encrypt(Long data) throws Exception {
+    private Cipher cipherInit(int mode) throws Exception {
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
         IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes());
-        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParameterSpec);
+        cipher.init(mode, keySpec, ivParameterSpec);
+        return cipher;
+    }
 
+    public String encrypt(Long data) throws Exception {
+        Cipher cipher = cipherInit(Cipher.ENCRYPT_MODE);
         byte[] encodedBytes = cipher.doFinal(data.toString().getBytes());
         byte[] encrypted = Base64.getEncoder().encode(encodedBytes);
         return new String(encrypted).trim();
     }
 
     public Long decrypt(String encryptedData) throws Exception {
-        Cipher cipher = Cipher.getInstance(ALGORITHM);
-        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
-        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes());
-        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParameterSpec);
-
+        Cipher cipher = cipherInit(Cipher.DECRYPT_MODE);
         byte[] decodedBytes = Base64.getDecoder().decode(encryptedData.getBytes());
         byte[] decrypted = cipher.doFinal(decodedBytes);
         return Long.parseLong(new String(decrypted, StandardCharsets.UTF_8));
