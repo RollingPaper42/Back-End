dc88a19f71b6fa472932191b6ec6e137ec294504
brixxt27 <brixxt27@gmail.com>
2023-11-19 17:09:31 +0900

    feat: BoardService 구현
    - 보드 조회 구현
    - 보드 생성 구현
    - readBoardInfo() 함수 삭제 예정

--- src/main/java/com/strcat/service/BoardService.java
+++ src/main/java/com/strcat/service/BoardService.java
@@ -3,10 +3,11 @@ package com.strcat.service;
 import com.strcat.domain.Board;
 import com.strcat.domain.User;
 import com.strcat.dto.CreateBoardReqDto;
+import com.strcat.dto.ReadBoardInfoResDto;
 import com.strcat.exception.NotAcceptableException;
 import com.strcat.repository.BoardRepository;
-import com.strcat.repository.OAuthUserRepository;
 import com.strcat.repository.UserRepository;
+import com.strcat.util.AesSecretUtils;
 import com.strcat.util.JwtUtils;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -18,8 +19,9 @@ public class BoardService {
     private final BoardRepository boardRepository;
     private final UserRepository userRepository;
     private final JwtUtils jwtUtils;
+    private final AesSecretUtils aesSecretUtils;
 
-    public void createBoard(CreateBoardReqDto dto, String token) {
+    public String createBoard(CreateBoardReqDto dto, String token) throws Exception {
         Long userId = Long.parseLong(jwtUtils.parseUserId(token));
         Optional<User> user = userRepository.findById(userId);
 
@@ -29,6 +31,29 @@ public class BoardService {
 
         // TODO: group id 유효성 검사
 
-        boardRepository.save(new Board(dto.getTitle(), dto.getBackgroundColor(), user.get()));
+        Board board = boardRepository.save(new Board(dto.getTitle(), dto.getBackgroundColor(), user.get()));
+        return aesSecretUtils.encrypt(board.getId());
+    }
+
+    // TODO: 삭제 예정
+    public ReadBoardInfoResDto readBoardInfo(String encryptedBoardId) throws Exception {
+        Long boardId = aesSecretUtils.decrypt(encryptedBoardId);
+        Optional<Board> optionalBoard = boardRepository.findById(boardId);
+
+        if (optionalBoard.isEmpty()) {
+            throw new NotAcceptableException();
+        }
+        Board board = optionalBoard.get();
+        return new ReadBoardInfoResDto(board.getTitle(), board.getBackgroundColor());
+    }
+
+    public Board readBoard(String encryptedBoardId) throws Exception {
+        Long boardId = aesSecretUtils.decrypt(encryptedBoardId);
+        Optional<Board> optionalBoard = boardRepository.findById(boardId);
+
+        if (optionalBoard.isEmpty()) {
+            throw new NotAcceptableException();
+        }
+        return optionalBoard.get();
     }
 }
