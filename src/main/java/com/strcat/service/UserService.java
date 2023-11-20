30f85262aec9bf62e87c0e2c5de41332cfa38d30
brixxt27 <brixxt27@gmail.com>
2023-11-20 22:31:38 +0900

    refactor: UserService 생성
    - 기존에 BoardService에 있던 함수를 추출하여 UserService를 구현
    - User라는 domain은 UserService에서만 접근 할 수 있도록 제한한다. User에 접근하는 방법은 UserRepository를 사용하는 것이다.

---
+++ src/main/java/com/strcat/service/UserService.java
@@ -0,0 +1,26 @@
+package com.strcat.service;
+
+import com.strcat.domain.User;
+import com.strcat.exception.NotAcceptableException;
+import com.strcat.repository.UserRepository;
+import com.strcat.util.JwtUtils;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final JwtUtils jwtUtils;
+
+    public User getUser(String token) {
+        Long userId = Long.parseLong(jwtUtils.parseUserId(token.substring(7)));
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            throw new NotAcceptableException();
+        }
+        return user.get();
+    }
+}
