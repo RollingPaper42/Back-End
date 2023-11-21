2011b8da0a97555a42eb9c0493bd2d7d7fe9e20c
seongjunkim <tjdwns5063@naver.com>
2023-11-21 18:55:05 +0900

    feat: 임시 api 구현
    - aws s3에 사진을 저장하는 임시 api 구현

--- src/main/java/com/strcat/controller/MainController.java
+++ src/main/java/com/strcat/controller/MainController.java
@@ -1,10 +1,20 @@
 package com.strcat.controller;
 
+import com.strcat.repository.PictureRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequiredArgsConstructor
+@Slf4j
 public class MainController {
+    private final PictureRepository pictureRepository;
+
     @GetMapping("/")
     public String helloSpring() {
         return "Hello spring!!!";
@@ -18,5 +28,12 @@ public class MainController {
     @GetMapping("/login/guard")
     public String guard() {
         return "guard OK";
+    }
+
+    @PostMapping("/picture/test")
+    public String test(@RequestParam("picture") MultipartFile picture) {
+        log.debug("picture API Called");
+        System.out.println("picture API Called");
+        return pictureRepository.postPicture(1L, 1L, picture);
     }
 }
