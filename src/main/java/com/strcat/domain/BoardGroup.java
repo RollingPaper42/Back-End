ed570ca6e0beb3bd50587f0f8b5e85b8800a0112
Sumin Lee <suminn0707@gmail.com>
2023-11-22 00:34:54 +0900

    feat: 그룹 entity 작성

---
+++ src/main/java/com/strcat/domain/BoardGroup.java
@@ -0,0 +1,54 @@
+package com.strcat.domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Data
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class BoardGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(length = 30, nullable = false)
+    private String title;
+
+    @OneToMany(mappedBy = "boardGroup", cascade = CascadeType.ALL)
+    private List<Board> boards;
+
+    public BoardGroup(String title, User user) {
+        this.title = title;
+        this.user = user;
+    }
+
+    public Long calculateTotalContentLength(List<Board> boards) {
+        return boards.stream()
+                .flatMap(board -> board.getContents().stream())
+                .mapToLong(content -> content.getText().length())
+                .sum();
+    }
+}
