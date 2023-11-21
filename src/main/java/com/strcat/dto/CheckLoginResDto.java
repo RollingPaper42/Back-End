1e0259ff8f8e3ca4acb932297d17dfcb054cd072
brixxt27 <brixxt27@gmail.com>
2023-11-21 13:32:27 +0900

    feat: 로그인 여부 확인 API 구현
    - lombok의 @Getter 어노테이션은 is로 시작하는 프로퍼티를 보면 자동으로 getter 를 isLogin() 이라는 이름의 메소드로 변경한다. 그리고 반환 할 때 프로퍼티 명에서 is를 떼어버리고 반환하여 isLogin이 아닌 login으로 반환하게 된다. 이는 @Getter를 사용하지 않으면 해결 된다.
    - 기존 isLogin 프로퍼티 명을 login으로 변경했다. 이유는 이후에 is~로 시작하는 부분이 또 dto에 생긴다면 그 부분에 대해 Getter를 따로 만들어 줘야 하기 때문이다. 그렇게 되면 @Getter를 사용하지 않고 모든 프로퍼티를 만들어 줘야 한다.
    - 프로퍼티가 많을 때 @Getter를 사용하면서 is~로 시작하는 프로퍼티만 게터를 따로 만드는 방법을 찾아볼 필요는 있어 보인다.

---
+++ src/main/java/com/strcat/dto/CheckLoginResDto.java
@@ -0,0 +1,10 @@
+package com.strcat.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CheckLoginResDto {
+    private final boolean login;
+}
