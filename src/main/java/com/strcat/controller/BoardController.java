9f5725a5c2ee8393485c338f5df885de13152a3b
seongjunkim <tjdwns5063@naver.com>
2023-11-21 18:55:30 +0900

    feat: 사진 저장 api 구현
    - aws s3에 사진을 저장하는 api 구현

--- src/main/java/com/strcat/controller/BoardController.java
+++ src/main/java/com/strcat/controller/BoardController.java
@@ -7,6 +7,7 @@ import com.strcat.dto.ReadBoardInfoResDto;
 import com.strcat.dto.ReadBoardSummaryResDto;
 import com.strcat.service.BoardService;
 import com.strcat.service.ContentService;
+import com.strcat.service.PictureService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/boards")
@@ -22,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
     private final ContentService contentService;
     private final BoardService boardService;
+    private final PictureService pictureService;
 
     @PostMapping("/{boardId}/contents")
     public void createContent(@PathVariable(name = "boardId") String encryptedBoardId,
@@ -50,5 +55,12 @@ public class BoardController {
     public ReadBoardSummaryResDto readSummary(@PathVariable(name = "boardId") String encryptedBoardId,
                                               @RequestHeader("Authorization") String token) {
         return boardService.readSummary(encryptedBoardId, token);
+    }
+//    http://localhost:8080/boards/Vvs_JTGorbxqVWXr6aH0cg==/contents/1/pictures
+    @PostMapping("/{boardId}/contents/{contentId}/pictures")
+    public String createPicture(@PathVariable(name = "boardId") String encryptedBoardId,
+                                @PathVariable String contentId,
+                                @RequestParam MultipartFile picture) {
+        return pictureService.postPicture(encryptedBoardId, contentId, picture);
     }
 }
