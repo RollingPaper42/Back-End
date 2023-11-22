a41d0356033fab66c3c92d05df96e38a3d9e14a8
brixxt27 <brixxt27@gmail.com>
2023-11-22 22:57:44 +0900

    feat: 내 정보 조회하기 서비스 구현
    - user가 유효한지 검사
    - user의 아이디를 가져와 해당하는 userId를 가진 모든 Board를 리스트로 가져와 반환

--- src/main/java/com/strcat/service/BoardService.java
+++ src/main/java/com/strcat/service/BoardService.java
@@ -2,11 +2,13 @@ package com.strcat.service;
 
 import com.strcat.domain.Board;
 import com.strcat.domain.User;
+import com.strcat.dto.ReadMyBoardInfoResDto;
 import com.strcat.dto.CreateBoardReqDto;
 import com.strcat.dto.ReadBoardSummaryResDto;
 import com.strcat.exception.NotAcceptableException;
 import com.strcat.repository.BoardRepository;
 import com.strcat.util.AesSecretUtils;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -38,6 +40,11 @@ public class BoardService {
         ReadBoardSummaryResDto dto = new ReadBoardSummaryResDto(board.getTitle(), board.getContents().size(),
                 board.calculateTotalContentLength());
         return dto;
+    }
+
+    public List<ReadMyBoardInfoResDto> readMyBoardInfo(String token) {
+        User user = userService.getUser(token);
+        return boardRepository.findByUserId(user.getId());
     }
 
     private Board getBoard(String encryptedBoardId) {
