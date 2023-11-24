dcd8c10d8ab67072587a906a5cef40059c2d8574
Sumin Lee <suminn0707@gmail.com>
2023-11-24 18:01:20 +0900

    feat: 보드 그룹 api 합침

--- src/main/java/com/strcat/controller/BoardGroupController.java
+++ src/main/java/com/strcat/controller/BoardGroupController.java
@@ -1,11 +1,9 @@
 package com.strcat.controller;
 
-import com.strcat.domain.Board;
 import com.strcat.dto.CreateBoardGroupReqDto;
 import com.strcat.dto.ReadBoardGroupResDto;
 import com.strcat.dto.ReadBoardGroupSummaryResDto;
 import com.strcat.service.BoardGroupService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,14 +25,9 @@ public class BoardGroupController {
         return boardGroupService.create(dto, token);
     }
 
-    @GetMapping("/{boardGroupId}")
+    @GetMapping("/{boardGroupId}/boards")
     public ReadBoardGroupResDto readBoardGroup(@PathVariable(name = "boardGroupId") String encryptedBoardGroupId) {
         return boardGroupService.readBoardGroup(encryptedBoardGroupId);
-    }
-
-    @GetMapping("/{boardGroupId}/boards")
-    public List<Board> readBoardGroupContent(@PathVariable(name = "boardGroupId") String encryptedBoardGroupId) {
-        return boardGroupService.readBoards(encryptedBoardGroupId);
     }
 
     @GetMapping("/{boardGroupId}/summaries")
