2c5d56ad6f4a4c926cf9438610272c4cad653724
brixxt27 <brixxt27@gmail.com>
2023-11-27 21:02:19 +0900

    refactor: AesSecretUtils에서 SecureDataUtils로 알고리즘에 의존적이지 않도록 이름을 변경함

--- src/main/java/com/strcat/service/BoardService.java
+++ src/main/java/com/strcat/service/BoardService.java
@@ -10,7 +10,7 @@ import com.strcat.dto.ReadMyBoardInfoResDto;
 import com.strcat.exception.NotAcceptableException;
 import com.strcat.repository.BoardGroupRepository;
 import com.strcat.repository.BoardRepository;
-import com.strcat.util.AesSecretUtils;
+import com.strcat.util.SecureDataUtils;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -22,7 +22,7 @@ import org.springframework.stereotype.Service;
 public class BoardService {
     private final BoardRepository boardRepository;
     private final BoardGroupRepository boardGroupRepository;
-    private final AesSecretUtils aesSecretUtils;
+    private final SecureDataUtils secureDataUtils;
     private final UserService userService;
 
     public List<Board> findByUserId(Long userId) {
@@ -33,7 +33,7 @@ public class BoardService {
         User user = userService.getUser(token);
         List<Board> boards = findByUserId(user.getId());
         return boards.stream()
-                .map(board -> new ReadMyBoardInfoResDto(aesSecretUtils.encrypt(
+                .map(board -> new ReadMyBoardInfoResDto(secureDataUtils.encrypt(
                         board.getId()),
                         board.getTitle(),
                         board.getTheme()))
@@ -49,7 +49,7 @@ public class BoardService {
         } else {
             board = boardRepository.save(new Board(dto.getTitle(), dto.getTheme(), user));
         }
-        return aesSecretUtils.encrypt(board.getId());
+        return secureDataUtils.encrypt(board.getId());
     }
 
     public ReadBoardResDto readBoard(String encryptedBoardId, String token) {
@@ -68,7 +68,7 @@ public class BoardService {
     }
 
     private Board getBoard(String encryptedBoardId) {
-        Long boardId = aesSecretUtils.decrypt(encryptedBoardId);
+        Long boardId = secureDataUtils.decrypt(encryptedBoardId);
         Optional<Board> optionalBoard = boardRepository.findById(boardId);
 
         if (optionalBoard.isEmpty()) {
@@ -78,7 +78,7 @@ public class BoardService {
     }
 
     private BoardGroup getBoardGroup(String encryptedBoardGroupId) {
-        Long boardGroupId = aesSecretUtils.decrypt(encryptedBoardGroupId);
+        Long boardGroupId = secureDataUtils.decrypt(encryptedBoardGroupId);
         Optional<BoardGroup> boardGroup = boardGroupRepository.findById(boardGroupId);
 
         if (boardGroup.isEmpty()) {
