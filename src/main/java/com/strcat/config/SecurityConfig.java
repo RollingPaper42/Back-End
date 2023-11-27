64f8cf9c0609f8c10aa8597504543bccac6dcbfb
seongjunkim <tjdwns5063@naver.com>
2023-11-27 13:30:36 +0900

    fix: WHITE_LIST에 board/{boardId}/contents 추가

--- src/main/java/com/strcat/config/SecurityConfig.java
+++ src/main/java/com/strcat/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
             "v3/api-docs/**",
             "swagger-ui/**",
             "boards/*",
+            "boards/*/contents",
             "boards/*/contents/pictures",
             "board-groups/*"
     };
