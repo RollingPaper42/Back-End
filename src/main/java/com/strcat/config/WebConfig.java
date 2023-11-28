cbcdc747095f20093e966654daec429702d6159e
seongjunkim <tjdwns5063@naver.com>
2023-11-29 00:41:52 +0900

    fix: cors origin localhost:3000으로 변경

--- src/main/java/com/strcat/config/WebConfig.java
+++ src/main/java/com/strcat/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
