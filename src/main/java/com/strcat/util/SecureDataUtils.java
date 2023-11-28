275d8d00d0f7dfcee0d67ffecde945c65540cc5d
brixxt27 <brixxt27@gmail.com>
2023-11-28 17:03:29 +0900

    feat: 암호화 구현 변경
    - 기존에 숫자를 단순하게 암호화 후 반환하게 되었는데 이 때 Board와 Board-group이 서로 도메인이 다름에도 불구하고 암호화 된 id를 유추할 수 있는 문제가 있었다. 이를 해결하기 위해 현재 시간을 넣어 암호화를 진행하기로 했다.
    - UTC 시간을 기준으로 문자열을 붙여서 암호화를 진행했으며 "," 기준으로 split 후 사용하게 된다.
    - 필드일 필요 없고, 지역변수로도 충분한 상수는 메서드 내로 넣었다.

--- src/main/java/com/strcat/util/SecureDataUtils.java
+++ src/main/java/com/strcat/util/SecureDataUtils.java
@@ -2,6 +2,7 @@ package com.strcat.util;
 
 import com.strcat.exception.NotAcceptableException;
 import java.nio.charset.StandardCharsets;
+import java.time.Instant;
 import java.util.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
@@ -11,20 +12,24 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SecureDataUtils {
-
     private final String key;
     private final String iv;
-    private final String ALGORITHM = "AES/CBC/PKCS5Padding";
 
     public SecureDataUtils(@Value("${jwt.secret}") String secretKey) {
         this.key = secretKey;
         this.iv = secretKey.substring(0, 16);
     }
 
+    private String getCurrentTime() {
+        return Instant.now().toString();
+    }
+
     public String encrypt(Long data) {
+        final String SEPARATOR = ",";
         try {
             Cipher cipher = cipherInit(Cipher.ENCRYPT_MODE);
-            byte[] encodedBytes = cipher.doFinal(data.toString().getBytes());
+            String input = data.toString() + SEPARATOR + getCurrentTime();
+            byte[] encodedBytes = cipher.doFinal(input.getBytes());
             byte[] encrypted = Base64.getEncoder().encode(encodedBytes);
             return new String(encrypted, StandardCharsets.UTF_8).replace('/', '_');
         } catch (Exception e) {
@@ -32,18 +37,23 @@ public class SecureDataUtils {
         }
     }
 
+    private Long getIdFromRawDataToLong(String rawData) {
+        return Long.parseLong(rawData.split(",")[0]);
+    }
+
     public Long decrypt(String encryptedData) {
         try {
             Cipher cipher = cipherInit(Cipher.DECRYPT_MODE);
             byte[] decodedBytes = Base64.getDecoder().decode(encryptedData.replace('_', '/').getBytes());
             byte[] decrypted = cipher.doFinal(decodedBytes);
-            return Long.parseLong(new String(decrypted, StandardCharsets.UTF_8));
+            return getIdFromRawDataToLong(new String(decrypted, StandardCharsets.UTF_8));
         } catch (Exception e) {
             throw new NotAcceptableException("복호화에 실패했습니다.");
         }
     }
 
     private Cipher cipherInit(int mode) throws Exception {
+        final String ALGORITHM = "AES/CBC/PKCS5Padding";
         Cipher cipher = Cipher.getInstance(ALGORITHM);
         SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
         IvParameterSpec ivParameterSpec = new IvParameterSpec(iv.getBytes());
