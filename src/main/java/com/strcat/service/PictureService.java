c745005b3826733d148fd07d10f5a7ae5b242656
seongjunkim <tjdwns5063@naver.com>
2023-11-29 13:39:26 +0900

    feat: multipart/form-data로 전송된 파일의 content-type 확인하는 로직 추가

--- src/main/java/com/strcat/service/PictureService.java
+++ src/main/java/com/strcat/service/PictureService.java
@@ -1,5 +1,6 @@
 package com.strcat.service;
 
+import com.strcat.exception.NotAcceptableException;
 import com.strcat.repository.PictureRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,7 +11,18 @@ import org.springframework.web.multipart.MultipartFile;
 public class PictureService {
     private final PictureRepository pictureRepository;
 
+    public boolean isInvalidContentType(String contentType) {
+        return switch (contentType) {
+            case "image/jpeg", "image/png" -> false;
+            default -> true;
+        };
+    }
+
     public String postPicture(String encryptedBoardId, MultipartFile picture) {
+        if (isInvalidContentType(picture.getContentType())) {
+            throw new NotAcceptableException("처리할 수 없는 파일 형식입니다.");
+        }
+
         return pictureRepository.postPicture(encryptedBoardId, picture);
     }
 }
