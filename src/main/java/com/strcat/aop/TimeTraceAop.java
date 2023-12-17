bdad92976e010fd8494093e8220ba32c27dd5f76
brixxt27 <brixxt27@gmail.com>
2023-12-17 14:53:31 +0900

    comment: 메서드 별 알아보기 편하게 주석 달기
    - 인자와 반환값, 예외 처리 등을 작성해 둠.

--- src/main/java/com/strcat/aop/TimeTraceAop.java
+++ src/main/java/com/strcat/aop/TimeTraceAop.java
@@ -8,22 +8,44 @@ import org.springframework.data.util.Pair;
 
 @Aspect
 public class TimeTraceAop {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(TimeTraceAop.class);
+
+    /**
+     * 밀리 세컨드의 단위로 시간을 측정하는 함수입니다.
+     * @return  Pair<Long, String>  시간과 단위를 반환합니다.
+     */
     private Pair<Long, String> fetchMilliSecond() {
         return Pair.of(System.currentTimeMillis(), "ms");
     }
 
+    /**
+     * 마이크로 세컨드의 단위로 시간을 측정하는 함수입니다.
+     * @return  Pair<Long, String>  시간과 단위를 반환합니다.
+     */
     private Pair<Long, String> fetchMicroSecond() {
         return Pair.of(System.nanoTime() / 1000, "μs");
     }
 
+    /**
+     * 나노 세컨드의 단위로 시간을 측정하는 함수입니다.
+     * @return  Pair<Long, String>  시간과 단위를 반환합니다.
+     */
     private Pair<Long, String> fetchNanoSecond() {
         return Pair.of(System.nanoTime(), "ns");
     }
 
-    @Around("execution(* com.strcat.controller..*(..)) || execution(* com.strcat.service..*(..))")
-    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
-        final String PREFIX = "\n-------> ";
-        // 함수 호출로 원하는 단위의 시간을 얻어올 수 있습니다.
+    /**
+     * AOP를 이용하여 함수의 수행 시간을 측정하는 함수입니다.
+     * @param joinPoint
+     * @return AOP에서 Around로 실행할 함수를 반환합니다.
+     * @throws Throwable
+     */
+    @Around("execution(* com.strcat.controller..*(..))"
+            + "|| execution(* com.strcat.service..*(..))")
+    public Object logMethodExecution(ProceedingJoinPoint joinPoint) throws Throwable {
+        final String PREFIX = "-------> ";
+        final String CALLED = "Called ";
+        final String BACK = "Back   ";
         Pair<Long, String> start = fetchMicroSecond();
 
         System.out.println(PREFIX + "호출 함수: " + joinPoint.toString());
