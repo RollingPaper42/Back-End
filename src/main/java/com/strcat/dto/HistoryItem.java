bd1fce7c8073448f3272cad21c93c7a7daeeb39b
seongjunkim <tjdwns5063@naver.com>
2024-02-07 17:02:47 +0900

    feat: 스웨거 가독성을 위해 @Schema 추가

--- src/main/java/com/strcat/dto/HistoryItem.java
+++ src/main/java/com/strcat/dto/HistoryItem.java
@@ -1,8 +1,16 @@
 package com.strcat.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 
+@Schema(example = """
+            {
+              "encryptedBoardId": "string",
+              "title": "string",
+              "visitTime": "2024-02-07 07:54:54"
+            }
+        """)
 public record HistoryItem(String encryptedBoardId, String title,
                           @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
                           LocalDateTime visitTime) {
